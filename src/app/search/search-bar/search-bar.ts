3a441a07515faa2f086ac00f903de728b91c49dc
Ilyavosky <ilyacortesruiz@gmail.com>
2025-10-22 18:55:00 -0600

    Actualización del componente de la barra de búsqueda

--- src/app/search/search-bar/search-bar.ts
+++ src/app/search/search-bar/search-bar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { SpotifySearchService } from '../services/spotify-api/spotify-search-service';
 
 @Component({
@@ -9,15 +9,26 @@ import { SpotifySearchService } from '../services/spotify-api/spotify-search-ser
 })
 export class SearchBar {
   searchQuery: string = '';
-
+  @Output() searchResults = new EventEmitter<any>();
+  @Output() searchInitiated = new EventEmitter<void>();
   constructor(
     private _spotifySearch: SpotifySearchService
   ){}
 
   doSearch(): void{
-    this._spotifySearch.doSearch(this.searchQuery).subscribe(
-      data => console.log(data)
-    )
+    if (this.searchQuery.trim() === '') {
+      this.searchResults.emit({ albums: { items: [] }, tracks: { items: [] } });
+      return;
+    }
+    this.searchInitiated.emit();
+    this._spotifySearch.doSearch(this.searchQuery).subscribe({
+      next: (data) => {
+        this.searchResults.emit(data);
+      },
+      error: (error) => {
+        console.error('Error during search:', error);
+        this.searchResults.emit(null);
+      }
+    });
   }
-
 } 
