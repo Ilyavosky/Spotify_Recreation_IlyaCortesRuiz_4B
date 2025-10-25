0e2e03e745c1f05e12b20decd6dbae45f19228d3
Ilyavosky <ilyacortesruiz@gmail.com>
2025-10-24 18:02:43 -0600

    Nueva lógica de busqueda

--- src/app/views/browse/browse.ts
+++ src/app/views/browse/browse.ts
@@ -1,45 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchModule } from '../../search/search-module';
 import { RouterLink} from '@angular/router';
+import { SearchStateService } from '../../services/state/search-state.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-browse',
   standalone: true,
   imports: [
     CommonModule,
-    SearchModule,
     RouterLink
   ],
   templateUrl: './browse.html',
   styleUrls: ['./browse.css']
 })
-export class BrowseComponent {
+export class BrowseComponent implements OnInit, OnDestroy{
   results: any = null;
   isLoading: boolean = false;
   hasError: boolean = false;
 
-  onSearchStarted(): void {
-    this.isLoading = true;
-    this.hasError = false;
-    this.results = null;
-    console.log("Search started...");
+  private subscriptions = new Subscription();
+
+ constructor(private searchStateService: SearchStateService) { }
+
+ngOnInit(): void {
+    this.subscriptions.add(
+        this.searchStateService.searchResults$.subscribe(results => {
+          this.results = results;
+          console.log('BrowseComponent received results via service:', results);
+        })
+    );
+    this.subscriptions.add(
+        this.searchStateService.isLoading$.subscribe(loading => {
+          this.isLoading = loading;
+          console.log('BrowseComponent isLoading via service:', loading);
+        })
+    );
+     this.subscriptions.add(
+        this.searchStateService.hasError$.subscribe(error => {
+          this.hasError = error;
+           console.log('BrowseComponent hasError via service:', error);
+        })
+     );
   }
 
-  handleSearchResults(data: any): void {
-    this.isLoading = false;
-    if (data && data.albums && data.tracks) {
-      console.log('Search results received:', data);
-      this.results = data;
-      this.hasError = false;
-    } else if (data === null) {
-       this.results = null;
-       this.hasError = true;
-       console.error('Received null results, indicating a search error.');
-    } else {
-        this.results = data;
-        this.hasError = false;
-         console.log('Received empty or unexpected results structure:', data);
-    }
+  ngOnDestroy(): void {
+      this.subscriptions.unsubscribe();
   }
+
 }
